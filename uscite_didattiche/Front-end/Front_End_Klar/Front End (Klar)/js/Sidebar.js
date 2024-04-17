e74045f4f8030f4f021481c622bc64d978b0911a
ADEX489 <489.adex@gmail.com>
2024-04-17 09:49:34 +0200

    Fix .custom-toggle-btn animation to open and close sidebar

--- uscite_didattiche/Front-end/Front_End_Klar/Front End (Klar)/js/Sidebar.js
+++ uscite_didattiche/Front-end/Front_End_Klar/Front End (Klar)/js/Sidebar.js
@@ -1,4 +1,14 @@
 function toggleMenu() {
     var sidebar = document.querySelector('.custom-sidebar');
-    sidebar.style.width = sidebar.style.width === '150px' ? '0' : '150px';
+    var toggleBtn = document.querySelector('.custom-toggle-btn');
+
+    if (sidebar.style.width === '150px') {
+      sidebar.style.width = '0';
+      toggleBtn.textContent = '☰';
+      toggleBtn.classList.remove('rotate'); // Rimuovi la classe di rotazione
+    } else {
+        sidebar.style.width = '150px';
+        toggleBtn.textContent = 'X';
+        toggleBtn.classList.add('rotate'); // Aggiungi la classe di rotazione
+    }
   }
