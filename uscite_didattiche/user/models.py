7ce6f7f83e6d0696fbb21802b77fe7a376bc9ec5
Samuele Passerini <samu.passe05@gmail.com>
2024-05-03 15:31:50 +0200

    added and edited all atributes of the db

--- uscite_didattiche/user/models.py
+++ uscite_didattiche/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
+from django.conf import settings
 
 class Gita(models.Model):
     STATO_CHOICES = (
@@ -7,18 +8,17 @@ class Gita(models.Model):
         ('IN_CORSO', 'In corso'),
         ('TERMINATA', 'Terminata'),
     )
-
-    Nome = models.CharField(max_length=20)
-    Prezzo = models.FloatField()
+    
+    
     Stato = models.CharField(max_length=20, choices=STATO_CHOICES, default='IN_PROGRAMMA')
-    Data_partenza = models.DateTimeField(null=True, blank=True)
-    Data_ritorno = models.DateTimeField(null=True, blank=True)
-    Luogo_partenza = models.CharField(max_length=20)
-    Luogo_ritorno = models.CharField(max_length=20)
+    Data_ritrovo = models.DateTimeField(null=True, blank=True)
+    Data_rientro = models.DateTimeField(null=True, blank=True)
+    Luogo_ritrovo = models.CharField(max_length=20)
+    Luogo_rientro = models.CharField(max_length=20)
     Proposta_Gita = models.ForeignKey('Proposta_Gita', on_delete=models.CASCADE)
-
+    
     def __str__(self):
-        return self.Nome
+        return self.Proposta_Gita.Titolo
         
     class Meta:
          verbose_name_plural = 'Gita'
@@ -29,15 +29,15 @@ class Proposta_Gita(models.Model):
         ('CONFERMATA', 'Confermata'),
         ('RIFIUTATA', 'Rifiutata'),
     )
-    Nome = models.CharField(max_length=20)
+    Titolo = models.CharField(max_length=20)
     Descrizione = models.CharField(max_length=300)
-    Data_partenza = models.DateTimeField(null=True, blank=True)
-    Luogo = models.CharField(max_length=20)
-    Prezzo = models.FloatField()
+    Data = models.DateTimeField(null=True, blank=True)
+    Posto = models.CharField(max_length=20)
+    Costo = models.FloatField()
     Stato = models.CharField(max_length=20, choices=STATO_CHOICES, default='IN_ELABORAZIONE')
 
     def __str__(self):
-        return self.Nome
+        return self.Titolo
 
     class Meta:
          verbose_name_plural = 'Proposte_Gita'
@@ -48,71 +48,68 @@ class Attività(models.Model):
         ('CONFERMATA', 'Confermata'),
         ('RIFIUTATA', 'Rifiutata'),
     )
-    Nome = models.CharField(max_length=20)
+    Titolo = models.CharField(max_length=20)
     Descrizione = models.CharField(max_length=300)
-    prezzo = models.FloatField()
-    
+    Costo = models.FloatField()
     Stato = models.CharField(max_length=20, choices=STATO_CHOICES, default='IN_ELABORAZIONE')
-
+    
     Proposta_Gita = models.ForeignKey('Proposta_Gita', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.Nome
+        return self.Titolo
         
     class Meta:
          verbose_name_plural = 'Attività'
 
 class Docenti_gita(models.Model):
-    Nome = models.CharField(max_length=20)
-    prezzo = models.FloatField()
-
+    Gita = models.ForeignKey('Gita', on_delete=models.CASCADE, default='')
+    utente = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default='')
+    
     def __str__(self):
-        return self.Nome
+        return "{} - {}".format(self.Gita, self.utente.username)
         
     class Meta:
          verbose_name_plural = 'Docenti_gita'
 
 class Documenti(models.Model):
-    Nome = models.CharField(max_length=20)
-    Descrizione = models.CharField(max_length=300)
-    Gita = models.ForeignKey('Gita', on_delete=models.CASCADE)
-    file = models.FileField(upload_to='documenti/')
+    Titolo = models.CharField(max_length=20)
+    Descrizione = models.CharField(max_length=300, default='')
+    Gita = models.ForeignKey('Gita', on_delete=models.CASCADE, default='')
+    Allegato = models.FileField(upload_to='documenti/',default='')
 
     def __str__(self):
-        return self.Nome
+        return self.Titolo
         
     class Meta:
          verbose_name_plural = 'Documenti'
 
 class Classe_gita(models.Model):
-    Nome = models.CharField(max_length=20)
-    prezzo = models.FloatField()
-
+    Gita = models.ForeignKey('Gita', on_delete=models.CASCADE, default='')
+    Classe = models.ForeignKey('Classe', on_delete=models.CASCADE, default='')
     def __str__(self):
-        return self.Nome
+        return "{} - {}".format(self.Classe, self.Gita)
         
     class Meta:
          verbose_name_plural = 'Classe_gita'
 
 
 class Classe(models.Model):
-    Nome = models.CharField(max_length=20)
-    anno = models.IntegerField(validators=[MaxValueValidator(5)])
-    sezione = models.CharField(max_length=3)
+    anno = models.IntegerField(validators=[MaxValueValidator(5)],default=0)
+    sezione = models.CharField(max_length=3,default='')
 
 
     def __str__(self):
-        return self.Nome
+        return "{}{}".format(self.anno, self.sezione)
         
     class Meta:
          verbose_name_plural = 'Classe'
 
 class Presenti_prenotati(models.Model):
-    Nome = models.CharField(max_length=20)
-    prezzo = models.FloatField()
-
+    Gita = models.ForeignKey('Gita', on_delete=models.CASCADE, default='')
+    utente = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default='')
+    
     def __str__(self):
-        return self.Nome
+        return "{} - {}".format(self.Gita, self.utente.username)
         
     class Meta:
-         verbose_name_plural = 'Presenti_prenotati'
+        verbose_name_plural = 'Presenti_prenotati'
