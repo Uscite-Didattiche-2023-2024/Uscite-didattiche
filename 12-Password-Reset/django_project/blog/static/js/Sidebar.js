ca4355dc16df509eac9384b9e395340c23bcad65
Alessandro Gregorio - Igreg02 <I.gregale@gmail.com>
2024-05-04 12:50:55 +0200

    Fix index after marge

--- 12-Password-Reset/django_project/blog/static/js/Sidebar.js
+++ 12-Password-Reset/django_project/blog/static/js/Sidebar.js
@@ -1,4 +1,29 @@
 function toggleMenu() {
-    var sidebar = document.querySelector('.custom-sidebar');
-    sidebar.style.width = sidebar.style.width === '150px' ? '0' : '150px';
+  var sidebar = document.querySelector('.custom-sidebar');
+  var toggleBtn = document.querySelector('.custom-toggle-btn');
+
+  if (sidebar.style.width === '150px') {
+    sidebar.style.width = '0';
+    toggleBtn.textContent = '☰';
+    toggleBtn.classList.remove('rotate'); // Rimuovi la classe di rotazione
+  } else {
+    sidebar.style.width = '150px';
+    toggleBtn.textContent = 'X';
+    toggleBtn.classList.add('rotate'); // Aggiungi la classe di rotazione
   }
+}
+
+
+function startshakebell() {
+
+  document.getElementById("Bell").className = "fas fa-bell fa-beat fa-shake";
+
+}
+
+
+function stopshakebell() {
+
+  document.getElementById("Bell").className = "fas fa-bell";
+
+  
+}
