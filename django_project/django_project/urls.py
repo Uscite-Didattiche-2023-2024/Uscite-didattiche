960c1704de97f209532eb138b11825c76a8ca156
Federico Busto <fede.corpe76@gmail.com>
2024-05-10 13:22:25 +0200

    "logout" fixed

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
