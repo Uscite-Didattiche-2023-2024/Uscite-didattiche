c720430aab12b960fe6de39b18124306e53a1316
Federico Busto <fedeskyblack625@gmail.com>
2024-05-23 13:28:22 +0200

    Mail insert fixed

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -4,11 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 from gite.models import Classe, User_classe
 
-
 class PasswordResetForm(forms.Form):
     from_email = forms.EmailField(label="From Email")
     to_email = forms.EmailField(label="To Email")
-
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -38,6 +36,12 @@ class UserRegisterForm(UserCreationForm):
             "dettagli",
         ]
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("L'email è già in uso.")
+        return email
+
     def save(self, commit=True):
         user = super(UserRegisterForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
@@ -57,14 +61,12 @@ class UserRegisterForm(UserCreationForm):
                 user_classe.save()
         return user
 
-
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name"]
-
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
