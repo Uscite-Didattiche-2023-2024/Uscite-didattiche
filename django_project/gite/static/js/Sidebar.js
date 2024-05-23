9ca14b8c551139660314c83156dfed1039de45f8
Federico Busto <fedeskyblack625@gmail.com>
2024-05-23 13:46:22 +0200

    Fixed sidebar closure

--- django_project/gite/static/js/Sidebar.js
+++ django_project/gite/static/js/Sidebar.js
@@ -12,3 +12,18 @@ function toggleMenu() {
     toggleBtn.classList.add('rotate'); // Aggiungi la classe di rotazione
   }
 }
+
+// Chiude la sidebar quando si clicca nei 3/4 destri dello schermo
+document.addEventListener('click', function(event) {
+  var sidebar = document.querySelector('.custom-sidebar');
+  var toggleBtn = document.querySelector('.custom-toggle-btn');
+  var screenWidth = window.innerWidth;
+
+  // Verifica se il click è nei 3/4 destri dello schermo
+  if (sidebar.style.width === '150px' && event.clientX > screenWidth / 9 && !sidebar.contains(event.target) && !toggleBtn.contains(event.target)) {
+    sidebar.style.width = '0';
+    toggleBtn.textContent = '☰';
+    toggleBtn.classList.remove('rotate'); // Rimuovi la classe di rotazione
+  }
+});
+
