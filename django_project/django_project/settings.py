baa0ddd2a09ead9fcdb09772fdc48490a99d81b6
Christian Bertini <chriqwerty2004@gmail.com>
2024-05-24 09:27:32 +0200

    fix sidebar

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost","10.3.24.13"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost","10.3.24.13","172.20.10.12"]
 
 
 # Application definition
